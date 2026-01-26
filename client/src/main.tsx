1005e371ec5de26c57a6d0bdf3ea3de5aeb4bbe9
RobNamias <cedric.rodien@hotmail.fr>
2026-01-27 00:17:11 +0100

    correction css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css' // Replaced by SCSS architecture
 import './styles/app.scss'
 import './i18n-setup';
 import App from './App.tsx'
